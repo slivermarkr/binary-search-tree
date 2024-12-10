e7661c466a26366154b92e0333ba78a1d080003c
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-12-10 19:37:00 +0800

    adding screenshots

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # binary-search-tree
+
+project link: https://www.theodinproject.com/lessons/javascript-binary-search-trees
+
+## test #1: Create a binary search tree from an array of random numbers < 100. You can create a function that returns an array of random numbers every time you call it if you wish.
+
+![Alt text](/screenshots/random-to-bst-isbalanced.jpg "random-to-bst")
+
+## test #2: Confirm that the tree is balanced by calling isBalanced.
+
+![Alt text](/screenshots/call-isbalanced.jpg)
+
+## test #3: Print out all elements in level, pre, post, and in order..
+
+### levelOrder traversal
+
+![Alt text](/screenshots/BFS.jpg)
+
+### inorder traversal: (left, root, right)
+
+![Alt text](/screenshots/inorder-traversal.jpg)
+
+### preorder traversal: (root, left, right)
+
+![Alt text](/screenshots/preorder-traversal.jpg)
+
+### postorder traversal: (left, right, root)
+
+![Alt text](/screenshots/postorder-traversal.jpg)
+
+## test #4: Unbalance the tree by adding several numbers > 100.
+
+![Alt text](/screenshots/unbalanced-tree.jpg)
+
+## test#5: Confirm that the tree is unbalanced by calling isBalanced.
+
+![Alt text](/screenshots/call-isbalanced.jpg)
+
+## test#6: Balance the tree by calling rebalance.
+
+## test#7: Confirm that the tree is balanced by calling isBalanced.
+
+![Alt text](/screenshots/rebalanced.jpg)
--- app.js
+++ app.js
@@ -2,19 +2,38 @@ import Tree from "./classes/tree.js";
 import randomSortedArray from "./components/random.js";
 import prettyPrint from "./components/prettyprint.js";
 import { justPrint, powTwo, doubled } from "./components/callbacks.js";
-const arr = randomSortedArray(10);
 const sample = [1, 3, 5, 8, 12, 16, 23, 47];
 
+const arr = randomSortedArray(12);
 const tree = new Tree(sample);
 
 tree.root = tree.buildTree(tree.arr);
+tree.insert(123);
+tree.insert(124);
+tree.insert(125);
+tree.insert(126);
+tree.insert(127);
+tree.rebalance();
+prettyPrint(tree.root);
+console.log("\n");
+console.log("tree.isBalanced() call:  ", tree.isBalanced());
+// let isBalance = tree.isBalanced(tree.root);
+// console.log("tree.isBalanced call: ", isBalance);
 // Breadth First Search or levelOrder
-// const cbJustPrint = tree.levelOrder(justPrint);
-// const cbDoubled = tree.levelOrder(doubled);
-// const cbPowTwo = tree.levelOrder(powTwo);
+// const cbJustPrint = tree.postOrder(justPrint);
+// const cbDoubled = tree.postOrder(doubled);
+// const cbPowTwo = tree.postOrder(powTwo);
+// prettyPrint(tree.root);
+// console.log("\n");
+// console.log("callback: (x) => x");
 // console.log(cbJustPrint(tree.root).join("->"));
+// console.log("\n");
+// console.log("callback: (x) => x * 2");
 // console.log(cbDoubled(tree.root).join("->"));
+// console.log("\n");
+// console.log("callback: (x) => x * x");
 // console.log(cbPowTwo(tree.root).join("->"));
+// console.log("\n");
 
 // Depth First Search
 // Inorder Traversal
@@ -41,22 +60,23 @@ tree.root = tree.buildTree(tree.arr);
 // console.log(preTraversePow(tree.root).join("->"));
 // console.log("\n");
 
-// preOrder Traversal
+// postOrder Traversal
+
 // const postTraversePrt = tree.postOrder(justPrint);
 // const postTraverseDou = tree.postOrder(doubled);
 // const postTraversePow = tree.postOrder(powTwo);
-
 // console.log(postTraversePrt(tree.root).join("->"));
 // console.log("\n");
 // console.log(postTraverseDou(tree.root).join("->"));
 // console.log("\n");
 // console.log(postTraversePow(tree.root).join("->"));
 // console.log("\n");
-prettyPrint(tree.root);
-tree.insert(59);
-tree.insert(69);
-tree.insert(90);
-tree.insert(120);
-prettyPrint(tree.root);
-tree.rebalance();
-prettyPrint(tree.root);
+
+// prettyPrint(tree.root);
+// tree.insert(59);
+// tree.insert(69);
+// tree.insert(90);
+// tree.insert(120);
+// prettyPrint(tree.root);
+// tree.rebalance();
+// prettyPrint(tree.root);
