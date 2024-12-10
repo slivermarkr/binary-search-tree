1a7309ea224f21489468b80758f3da6095dc5497
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-12-10 15:20:10 +0800

    class Tree insert method

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import Tree from "./classes/tree.js";
-import sortedArray from "./components/random.js";
+import randomSortedArray from "./components/random.js";
 import prettyPrint from "./components/prettyprint.js";
 
+const arr = randomSortedArray(10);
 const sample = [1, 3, 5, 8, 12, 16, 23, 47];
-const arr = sortedArray(10);
+
 const tree = new Tree(sample);
 tree.root = tree.buildTree(tree.arr);
+tree.insert(2);
 prettyPrint(tree.root);
--- classes/tree.js
+++ classes/tree.js
@@ -6,6 +6,7 @@ export default class Tree {
     this.root = null;
   }
   getArr = () => this.arr;
+  getRoot = () => this.root;
 
   buildTree(array = this.getArr(), start = 0, end = array.length - 1) {
     if (start > end) return null;
@@ -15,6 +16,17 @@ export default class Tree {
 
     root.left = this.buildTree(array, start, mid - 1);
     root.right = this.buildTree(array, mid + 1, end);
+    return root;
+  }
+
+  insert(value, root = this.getRoot()) {
+    if (root === null) return new Node(value);
+
+    if (root.data > value) {
+      root.left = this.insert(value, root.left);
+    } else if (root.data < value) {
+      root.right = this.insert(value, root.right);
+    }
     return root;
   }
 }
